c2442e22dfebdf6bdbdcd78a954164140284d025
ahmetselim1 <selim_pcf@hotmail.com>
2021-11-20 23:07:02 -0500

    last of the last 2

--- src/test/java/crossBrowser/VerifyTitle.java
+++ src/test/java/crossBrowser/VerifyTitle.java
@@ -17,7 +17,7 @@ public class VerifyTitle {
 
     @Test
     @Parameters("browser")
-    public void verifyPageTitle(String browserName) {
+    public void verifyPageTitle(String browserName) throws InterruptedException {
 
         if (browserName.equalsIgnoreCase("firefox")) {
             System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver.exe");
@@ -36,15 +36,15 @@ public class VerifyTitle {
         System.out.println("driver.getTitle() = " + driver.getTitle());
         String temperature = driver.findElement(By.id("temperature")).getText();
         System.out.println("temperature = " + temperature);
-        temperature = temperature.substring(0, 1);
+        temperature = temperature.substring(0, 2);
 
 
         try {
             int number = Integer.parseInt(temperature);
             System.out.println(number);
-            if (number > 20) {
+            if (number > 34) {
                 driver.findElement(By.xpath("//button[contains(text(),'Buy moisturizers')]")).click();
-            } else if (number < 20) {
+            } else if (number < 19) {
                 driver.findElement(By.xpath("//button[contains(text(),'Buy sunscreens')]")).click();
             }
         } catch (NumberFormatException ex) {
@@ -54,8 +54,13 @@ public class VerifyTitle {
         List<WebElement> elements = driver.findElements(By.xpath("//button[contains(text(),'Add')]"));
         elements.get(0).click();
         driver.findElement(By.xpath("//button[@onclick='goToCart()']")).click();
-
-
+        driver.findElement(By.xpath("//span[contains(text(),'Pay with Card')]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("email")).sendKeys("omertalha@gmail.com");
+        driver.findElement(By.id("card_number")).sendKeys("4242424242424242");
+        driver.findElement(By.id("cc-exp")).sendKeys("10/25");
+        driver.findElement(By.id("cc-csc")).sendKeys("125");
+        driver.findElement(By.xpath("//span[@class='iconTick']")).click();
         driver.quit();
     }
 
