1fb92c4a1396eb9906340d9655bd796834e3c66d
ahmetselim1 <selim_pcf@hotmail.com>
2021-11-20 22:49:12 -0500

    last of the last 1

--- src/test/java/crossBrowser/VerifyTitle.java
+++ src/test/java/crossBrowser/VerifyTitle.java
@@ -1,11 +1,14 @@
 package crossBrowser;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.GeckoDriverInfo;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class VerifyTitle {
 
@@ -14,25 +17,45 @@ public class VerifyTitle {
 
     @Test
     @Parameters("browser")
-    public void verifyPageTitle(String browserName){
+    public void verifyPageTitle(String browserName) {
 
-        if(browserName.equalsIgnoreCase("firefox")){
+        if (browserName.equalsIgnoreCase("firefox")) {
             System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver.exe");
             driver = new FirefoxDriver();
 
-        }else if(browserName.equalsIgnoreCase("chrome")){
-            System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
+        } else if (browserName.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
             driver = new ChromeDriver();
-
         }
 
 
         driver.manage().window().maximize();
 
 
-
-        driver.get("https://google.com");
+        driver.get("https://weathershopper.pythonanywhere.com/");
         System.out.println("driver.getTitle() = " + driver.getTitle());
+        String temperature = driver.findElement(By.id("temperature")).getText();
+        System.out.println("temperature = " + temperature);
+        temperature = temperature.substring(0, 1);
+
+
+        try {
+            int number = Integer.parseInt(temperature);
+            System.out.println(number);
+            if (number > 20) {
+                driver.findElement(By.xpath("//button[contains(text(),'Buy moisturizers')]")).click();
+            } else if (number < 20) {
+                driver.findElement(By.xpath("//button[contains(text(),'Buy sunscreens')]")).click();
+            }
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+        }
+
+        List<WebElement> elements = driver.findElements(By.xpath("//button[contains(text(),'Add')]"));
+        elements.get(0).click();
+        driver.findElement(By.xpath("//button[@onclick='goToCart()']")).click();
+
+
         driver.quit();
     }
 
